5c97a43f85f3e65e1ce609784c39a5bd233c8871
Holger Will <h.will@klimapartner.de>
2020-03-10 06:40:42 +0100

    rename to test multiple dots

--- tools/dev-server/index.js
+++ tools/dev-server/index.js
@@ -4,20 +4,33 @@ const fs = require('fs')
 const Koa = require('koa')
 const app = new Koa()
 
-fs.readdirSync('./lambdas').forEach(lambda => {
-  let config = JSON.parse(fs.readFileSync(`./lambdas/${lambda}/serverless.json`, 'utf-8'))
-  for (name in config) {
-    config[name].events.forEach(evt => {
-      console.log(evt.http)
-      const handler = require(`../../lambdas/${lambda}/index.js`).handler
-      router.addRoute(evt.http.method.toUpperCase(), `${evt.http.path}`, async (ctx, next) => {
-        ctx.body = await handler(ctx)
-        await next()
-      })
-    })
-  }
-})
+// fs.readdirSync('./lambdas').forEach(lambda => {
+//   let config = JSON.parse(fs.readFileSync(`./lambdas/${lambda}/serverless.json`, 'utf-8'))
+//   for (name in config) {
+//     config[name].events.forEach(evt => {
+//       console.log(evt.http)
+//       const handler = require(`../../lambdas/${lambda}/index.js`).handler
+//       router.addRoute(evt.http.method.toUpperCase(), `${evt.http.path}`, async (ctx, next) => {
+//         ctx.body = await handler(ctx)
+//         await next()
+//       })
+//     })
+//   }
+// })
+
+function splitHandler (handler) {
+  return [handler.split('.')[0]]
+}
+
+let config = JSON.parse(fs.readFileSync(`serverless.json`, 'utf-8'))
+for (func in config.functions) {
+  const path = config.functions[func].replace('${file(', '').replace(')}', '')
+  let fconf = JSON.parse(fs.readFileSync(path, 'utf-8'))
+  console.log(splitHandler(fconf[func].handler))
+}
+
 app.use(router.middleware())
+
 app.use((ctx, next) => {
   ctx.status = ctx.body.statusCode
   for (header in ctx.body.headers) {
