5276aac6a6c609de2f4fbfa3132991879f38413d
Holger Will <h.will@klimapartner.de>
2020-03-10 08:20:34 +0100

    aws event transcoder v0.0.1

--- tools/dev-server/index.js
+++ tools/dev-server/index.js
@@ -1,22 +1,9 @@
-let Router = require('koa-better-router')
-let router = Router().loadMethods()
+const Router = require('koa-better-router')
+const router = Router().loadMethods()
 const fs = require('fs')
+const awsTranscoder = require('./modules/aws-transcoder.js')
 const Koa = require('koa')
 const app = new Koa()
-
-// fs.readdirSync('./lambdas').forEach(lambda => {
-//   let config = JSON.parse(fs.readFileSync(`./lambdas/${lambda}/serverless.json`, 'utf-8'))
-//   for (name in config) {
-//     config[name].events.forEach(evt => {
-//       console.log(evt.http)
-//       const handler = require(`../../lambdas/${lambda}/index.js`).handler
-//       router.addRoute(evt.http.method.toUpperCase(), `${evt.http.path}`, async (ctx, next) => {
-//         ctx.body = await handler(ctx)
-//         await next()
-//       })
-//     })
-//   }
-// })
 
 function splitHandler (handler) {
   const r = handler.split('.')
@@ -37,13 +24,9 @@ for (func in config.functions) {
   })
 }
 
+app.use(awsTranscoder.request)
 app.use(router.middleware())
+app.use(awsTranscoder.response)
 
-app.use((ctx, next) => {
-  ctx.status = ctx.body.statusCode
-  for (header in ctx.body.headers) {
-    ctx.set(header, ctx.body.headers[header])
-  }
-  ctx.body = ctx.body.body
-})
 app.listen(3322)
+
