df101ee54c28c3b5c890036f82166b646e96b606
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-03-12 10:50:05 +0100

    Moved to unique layer usage for API

--- tools/install-layers-dependencies/install-layers-dependencies.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs')
-const childProc = require('child_process')
-
-fs.readdirSync('layers').forEach(layer => {
-  childProc.execSync(`cd layers/${layer}/nodejs && npm i && cd ../../../`)
-})
