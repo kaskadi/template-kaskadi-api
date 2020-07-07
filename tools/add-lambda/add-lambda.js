b68851bb1d0a91c01a59f6abea8c22b6c71ef5df
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-07-07 16:29:29 +0200

    Updated folder structure

--- tools/add-lambda/add-lambda.js
+++ tools/add-lambda/add-lambda.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
-const checkArgs = require('../helpers/check-args.js')
-const getArgs = require('../helpers/get-args.js')
-const createFolders = require('../helpers/create-folders.js')
-const copyFiles = require('../helpers/copy-files.js')
-const updateFiles = require('../helpers/update-files.js')
-const appendLambda = require('../helpers/append.js')
+const checkArgs = require('./helpers/check-args.js')
+const getArgs = require('./helpers/get-args.js')
+const createFolders = require('./helpers/create-folders.js')
+const copyFiles = require('./helpers/copy-files.js')
+const updateFiles = require('./helpers/update-files.js')
+const appendLambda = require('./helpers/append.js')
 
 const args = process.argv.slice(2)
 const argsName = ['name', 'method', 'path']
@@ -13,7 +13,7 @@ checkArgs({
   semIndex: ['first', 'second', 'third']
 }, args)
 const data = getArgs(argsName, args)
-const pathToTool = 'tools/add/add-lambda'
+const pathToTool = 'tools/add-lambda'
 const pathToLambda = `lambdas/${data.name}`
 createFolders([pathToLambda], fs)
 copyFiles([
